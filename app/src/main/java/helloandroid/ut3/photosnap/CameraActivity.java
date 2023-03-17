0a34e7f25914ba106a80b2c11e776ed60806caf3
Raphael Huc <raph82130@gmail.com>
2023-03-17 09:57:57 +0100

    feat: add camera sensor and navigation to the gameActivity with the send the bitmap

--- app/src/main/java/helloandroid/ut3/photosnap/CameraActivity.java
+++ app/src/main/java/helloandroid/ut3/photosnap/CameraActivity.java
@@ -1,22 +1,24 @@
 package helloandroid.ut3.photosnap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class CameraActivity extends AppCompatActivity {
 
     private static final int CAMERA_REQUEST = 1888;
     private ImageView imageView;
+    private Bitmap photo;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
-    private TextView resultPhoto;
 
 
     @Override
@@ -24,11 +26,9 @@ public class CameraActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
         imageView = findViewById(R.id.cameraView);
-//        Button photoButton = this.findViewById(R.id.buttonPicture);
-//        resultPhoto = this.findViewById(R.id.resultPhoto);
     }
 
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
     {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == MY_CAMERA_PERMISSION_CODE)
@@ -46,16 +46,31 @@ public class CameraActivity extends AppCompatActivity {
         }
     }
 
-    public void onClick(View v)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-        if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
         {
+            photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+
+    public void onClickTakePhoto(View v)
+    {
+        if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
         }
         else {
             Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-
             startActivityForResult(cameraIntent, CAMERA_REQUEST);
         }
+    }
+
+    public void onClickStartGame(View v) {
+        Intent intent = new Intent(this, GameActivity.class);
+        intent.putExtra("bitmap", photo);
+        startActivity(intent);
     }
 }
